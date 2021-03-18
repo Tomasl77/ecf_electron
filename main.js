7f0cd74c70b4951f385c06be13dcc93d6791f5a3
Tomas Lobgeois <lobgeois.tomas@free.fr>
2021-03-18 13:16:16 +0100

    hide menu bar

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow () {
     width: 800,
     height: 600,
     fullscreen: true,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
